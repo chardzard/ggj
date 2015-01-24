86fadee56eb1d2db772f7c67d50b9c7b2a826722
Richard Sage <richard.sage28@gmail.com>
2015-01-24 11:39:39 -0400

    Basic move script on enemy with navmesh

---
+++ Assets/Scripts/Enemy AI/BasicNav.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicNav : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent agent;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update () {
+        agent.SetDestination(target.position);
+	}
+}
