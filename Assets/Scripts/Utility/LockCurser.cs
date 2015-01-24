24a388f5ca94552a34c5e002ef503d6c59a70cf7
Tormyst <raphael@celraph.com>
2015-01-24 11:09:36 -0400

    Lock Curser added
    Locks curser to screen.  Use escape to unlock.

---
+++ Assets/Scripts/Utility/LockCurser.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockCurser : MonoBehaviour {
+
+    void DidLockCursor() {
+        Debug.Log("Locking cursor");
+    }
+    void DidUnlockCursor() {
+        Debug.Log("Unlocking cursor");
+    }
+    void OnMouseDown() {
+        Screen.lockCursor = true;
+    }
+    private bool wasLocked = false;
+
+    public void Start()
+    {
+        Screen.lockCursor = true;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("escape"))
+            Screen.lockCursor = false;
+        
+        if (!Screen.lockCursor && wasLocked) {
+            wasLocked = false;
+            DidUnlockCursor();
+        } else
+            if (Screen.lockCursor && !wasLocked) {
+                wasLocked = true;
+                DidLockCursor();
+            }
+    }
+}
