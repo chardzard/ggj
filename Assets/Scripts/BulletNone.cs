acdbd5401ce06738cc6831648be08112b5a8c22f
Tormyst <raphael@celraph.com>
2015-01-24 13:40:27 -0400

    Bullet Fun
    If you dont like fun, you dont like Bullets.

---
+++ Assets/Scripts/BulletNone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletNone : MonoBehaviour {
+
+    public float m_Force;
+
+	void Start () {
+        rigidbody.AddForce(transform.forward * m_Force);
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+
+        }
+        else if(other.tag == "Enemy")
+        {
+
+        }
+        Destroy(gameObject);
+    }
+}
