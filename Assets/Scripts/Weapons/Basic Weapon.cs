9b00bc77ffd784a6f3a35c0c3a409147d70e5ad2
Gareth Yawching-Rickwood <GarethRickwood@hotmail.com>
2015-01-24 12:39:51 -0400

    Weapon abstraction script setup.

---
+++ Assets/Scripts/Weapons/Basic Weapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicWeapon : AbstractWeapon {
+
+	// Use this for initialization
+	void Start () {
+		base.maxAmmo = 30;
+		base.currentAmmo = base.maxAmmo;
+		base.fireRate = 10;
+		base.suggestedRange = 100;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void Fire() {
+		base.Fire ();
+	}
+}
