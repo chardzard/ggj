7727abbcf06834a62e9edf30b1d6c0f08611633b
Tormyst <raphael@celraph.com>
2015-01-23 22:37:21 -0400

    Added Gun and Gun Stuff
    Including some scripts that are basicly empty.

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour {
+
+    private PowerupTypes attack;
+
+    private PowerupTypes defence;
+
+    public PowerupTypes Attack 
+    {
+        get
+        {
+            return attack;
+        } 
+        set
+        {
+            attack = value;
+        }
+    }
+
+    public PowerupTypes Defence
+    {
+        get
+        {
+            return defence;
+        }
+        set
+        {
+            defence = value;
+        }
+    }
+}
---
+++ Assets/Scripts/PowerupEnum.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PowerupTypes
+{
+    none,
+    shield,
+    lazer,
+    bomb,
+    fire,
+    rapid,
+    burst
+}
