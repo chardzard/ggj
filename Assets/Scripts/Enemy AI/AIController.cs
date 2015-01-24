a58669d86c40011bf38a0beefd1144b9948ef688
Richard Sage <richard.sage28@gmail.com>
2015-01-24 14:22:25 -0400

    Shield type stuff

---
+++ Assets/Scripts/Enemy AI/AIController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+    GameObject agent;
+    public int health;
+
+	void Start () {
+        
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Pickups/PickupController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupController : MonoBehaviour {
+
+    public PowerupTypes type;
+
+    public PowerupTypes Type
+    {
+        get
+        {
+            return type;
+        }
+        set
+        {
+            type = value;
+        }
+    }
+
+}
