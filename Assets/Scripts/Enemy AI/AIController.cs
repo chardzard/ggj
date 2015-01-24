02289391a0d5ee2c42ef8907a7a081fcde7b1591
Gareth Yawching-Rickwood <GarethRickwood@hotmail.com>
2015-01-24 15:59:26 -0400

    Fixed weapon api setup, added non-working ai aiming/firing.

--- Assets/Scripts/Enemy AI/AIController.cs
+++ Assets/Scripts/Enemy AI/AIController.cs
@@ -5,12 +5,18 @@ public class AIController : MonoBehaviour {
 
     GameObject agent;
     public int health;
+	GameObject player;
+	AbstractWeapon weapon;
 
 	void Start () {
-        
+		player = GameObject.FindGameObjectWithTag("Player");
+		//weapon = Instantiate(new BasicWeapon(), transform.position + new Vector3(10, 0, -5), transform.rotation) as AbstractWeapon;
 	}
 	
 	void Update () {
-	
+		//Test firing stuff - aim at player and shoot
+	/*	if ((transform.position - player.transform.position).magnitude < weapon.SuggestedRange) {
+			weapon.Fire(gameObject);
+		}*/
 	}
 }
--- Assets/Scripts/Weapons/AbstractWeapon.cs
+++ Assets/Scripts/Weapons/AbstractWeapon.cs
@@ -4,17 +4,18 @@ using System.Collections;
 public abstract class AbstractWeapon : MonoBehaviour {
 	protected int maxAmmo;
 	protected int currentAmmo;
-	protected int fireRate;
-	protected int suggestedRange;
-
+	protected float fireRate;
+	protected float suggestedRange;
+	protected Transform gunPoint;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 	public int MaxAmmo {
 		get { return maxAmmo; }
@@ -23,17 +24,22 @@ public abstract class AbstractWeapon : MonoBehaviour {
 	public int CurrentAmmo {
 		get { return currentAmmo; }
 	}
-
-	public int FireRate {
+	
+	public float FireRate {
 		get { return fireRate; }
 	}
-
-	public int SuggestedRange {
+	
+	public float SuggestedRange {
 		get { return suggestedRange; }
 		set { suggestedRange = value; }
 	}
+
+	public Transform GunPoint {
+		get { return gunPoint; }
+	}
 	
-	protected void Fire() {
+	public bool Fire(GameObject shooter) {
 		currentAmmo--;
+		return true;
 	}
 }
