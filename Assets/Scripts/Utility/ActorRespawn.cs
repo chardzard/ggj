a1a0cdc70053314ba6322e07bccfbc3175f22465
Richard Sage <richard.sage28@gmail.com>
2015-01-25 14:39:32 -0400

    Added prefabs for pickups

---
+++ Assets/Scripts/Utility/ActorRespawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActorRespawn : MonoBehaviour {
+
+    public Vector3 location;
+
+    void Start()
+    {
+        location = transform.position;
+    }
+
+}
