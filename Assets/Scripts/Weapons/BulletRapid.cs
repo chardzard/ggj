531d4765b1a50de71a78cfbdb864f7772c6803b5
Tormyst <raphael@celraph.com>
2015-01-25 12:28:43 -0400

    Rapid Weapon Added

---
+++ Assets/Scripts/Weapons/BulletRapid.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletRapid : Bullet {
+
+    public float m_Force;
+
+    void Start()
+    {
+        rigidbody.velocity += transform.forward * m_Force;
+        StartCoroutine(DestroyForTime());
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == m_Shooter)
+        {
+            return;
+        }
+        if (other.tag == "Player")
+        {
+            PlayerHealthController hit = other.GetComponent<PlayerHealthController>();
+            hit.Helth -= 1;
+        }
+        else if (other.tag == "Enemy")
+        {
+            AIController hit = other.GetComponent<AIController>();
+            hit.health -= 1;
+        }
+        Destroy(gameObject);
+    }
+
+    private IEnumerator DestroyForTime()
+    {
+        yield return new WaitForSeconds(10f);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponRapid.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponRapid : AbstractWeapon {
+
+    private float lastShotTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        base.maxAmmo = 100;
+        base.currentAmmo = base.maxAmmo;
+        base.fireRate = 0.1f;
+        base.suggestedRange = 100;
+        base.gunPoint = transform.FindChild("Gunpoint");
+    }
+
+    public override bool Fire(GameObject shooter, Quaternion rotation, Vector3 velocity)
+    {
+        if (Time.time - lastShotTime > fireRate)
+        {
+            Debug.Log("ammo is " + currentAmmo);
+            currentAmmo--;
+            Bullet bullet = Instantiate(bulletPrefab, gunPoint.position, rotation) as Bullet;
+            bullet.m_Shooter = shooter;
+            bullet.rigidbody.velocity += velocity;
+            lastShotTime = Time.time;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
