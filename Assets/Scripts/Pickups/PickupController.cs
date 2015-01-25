1a83580fd4e9d4e950a0a6e5c05329ef304a982d
Richard Sage <richard.sage28@gmail.com>
2015-01-24 20:47:18 -0400

    Refactored pickup controller

--- Assets/Scripts/Pickups/PickupController.cs
+++ Assets/Scripts/Pickups/PickupController.cs
@@ -10,20 +10,6 @@ public class PickupController : MonoBehaviour {
         if (other.gameObject.tag == "Enemy")
         {
             AIController enemy = other.GetComponent<AIController>();
-            GameObject[] spawners = GameObject.FindGameObjectsWithTag("Spawner");
-            Vector3 closest = spawners[0].transform.position;
-            GameObject closestSpawn = null;
-            foreach (GameObject obj in spawners)
-            {
-                Vector3 currentPos = obj.transform.position - transform.position;
-                if (currentPos.magnitude < closest.magnitude)
-                {
-                    closest = currentPos;
-                    closestSpawn = obj;
-                }
-            }
-            Respawn spawn = closestSpawn.GetComponent<Respawn>();
-            spawn.startTimer();
             int weaponOrAmor = Random.Range(0, 2);
             if (weaponOrAmor == 0)
             {
@@ -33,7 +19,30 @@ public class PickupController : MonoBehaviour {
             {
                 enemy.setInventoryArmor(type);
             }
-            Destroy(gameObject);
         }
+        else if (other.gameObject.tag == "PlayerController")
+        {
+            //TODO functionality for players picking up powerup
+        }
+        spawnTimer();
+        Destroy(gameObject);
+    }
+
+    void spawnTimer()
+    {
+        GameObject[] spawners = GameObject.FindGameObjectsWithTag("Spawner");
+        Vector3 closest = spawners[0].transform.position;
+        GameObject closestSpawn = null;
+        foreach (GameObject obj in spawners)
+        {
+            Vector3 currentPos = obj.transform.position - transform.position;
+            if (currentPos.magnitude < closest.magnitude)
+            {
+                closest = currentPos;
+                closestSpawn = obj;
+            }
+        }
+        Respawn spawn = closestSpawn.GetComponent<Respawn>();
+        spawn.startTimer();
     }
 }
