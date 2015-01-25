0015ea1ac273fcc359194313d73ca2c91896994f
Tormyst <raphael@celraph.com>
2015-01-25 12:13:13 -0400

    Laser Weapon added

---
+++ Assets/Scripts/Weapons/BulletLaser.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletLaser : Bullet {
+
+    public Material startingMat;
+
+    public float fadeTime;
+
+    private LineRenderer lineRenderer;
+
+    private float totalTime;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,transform.forward,out hit))
+        {
+            lineRenderer.material = startingMat;
+            lineRenderer.SetVertexCount(2);
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, hit.point);
+
+            if(hit.transform.tag == "Player")
+            {
+                PlayerHealthController health = hit.transform.GetComponent<PlayerHealthController>();
+                health.Helth -= 35;
+            }
+            else if (hit.transform.tag == "Enemy")
+            {
+                AIController health = hit.transform.GetComponent<AIController>();
+                health.health -= 35;
+            }
+
+            totalTime = 0f;
+        }
+    }
+
+    public void Update()
+    {
+        totalTime += Time.deltaTime;
+        float fadelv = totalTime / fadeTime;
+        Color laserColor = Color.Lerp(Color.white, Color.clear, fadelv);
+        lineRenderer.SetColors(laserColor, laserColor);
+        if (fadelv >= 1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /*
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == m_Shooter || other.gameObject.tag == "Bullet")
+        {
+            return;
+        }
+        if (other.tag == "Player")
+        {
+            PlayerHealthController hit = other.GetComponent<PlayerHealthController>();
+            hit.Helth -= 3;
+        }
+        else if (other.tag == "Enemy")
+        {
+            AIController hit = other.GetComponent<AIController>();
+            hit.health -= 3;
+        }
+        Destroy(gameObject);
+    }
+     * */
+}
+
---
+++ Assets/Scripts/Weapons/WeaponLaser.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponLaser : AbstractWeapon {
+
+    private float lastShotTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        base.maxAmmo = 10;
+        base.currentAmmo = base.maxAmmo;
+        base.fireRate = 3f;
+        base.suggestedRange = 200;
+        base.gunPoint = transform.FindChild("Gunpoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override bool Fire(GameObject shooter, Quaternion rotation, Vector3 velocity)
+    {
+        if (Time.time - lastShotTime > fireRate)
+        {
+            lastShotTime = Time.time;
+            Debug.Log("ammo is " + currentAmmo);
+            currentAmmo--;
+            Bullet bullet = Instantiate(bulletPrefab, gunPoint.position, rotation) as Bullet;
+            bullet.m_Shooter = shooter;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public override int CurrentAmmo
+    {
+        get { return 42; }
+    }
+}
