f6e09a2e065b4803fbe7ccf6a48c652ddb8ee611
Tormyst <raphael@celraph.com>
2015-01-25 00:08:31 -0400

    Added Burst Weapon

---
+++ Assets/Scripts/Weapons/BulletBurst.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBurst : Bullet
+{
+
+    public float m_Force;
+
+    void Start()
+    {
+        rigidbody.velocity += transform.forward * m_Force;
+        StartCoroutine(DestroyForTime());
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == m_Shooter || other.gameObject.tag == "Bullet")
+        {
+            return;
+        }
+        if (other.tag == "Player")
+        {
+            PlayerHealthController hit = other.GetComponent<PlayerHealthController>();
+            hit.Helth -= 3;
+        }
+        else if (other.tag == "Enemy")
+        {
+            AIController hit = other.GetComponent<AIController>();
+            hit.health -= 3;
+        }
+        Destroy(gameObject);
+    }
+
+    private IEnumerator DestroyForTime()
+    {
+        yield return new WaitForSeconds(10f);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponBurst.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponBurst : AbstractWeapon
+{
+    private float lastShotTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        base.maxAmmo = 30;
+        base.currentAmmo = base.maxAmmo;
+        base.fireRate = 2f;
+        base.suggestedRange = 50;
+        base.gunPoint = transform.FindChild("Gunpoint");
+    }
+
+    public override bool Fire(GameObject shooter, Quaternion rotation, Vector3 velocity)
+    {
+        if (Time.time - lastShotTime > fireRate)
+        {
+            Debug.Log("ammo is " + currentAmmo);
+            currentAmmo--;
+            gunPoint.rotation = rotation;
+            for (int i = 0; i < gunPoint.childCount; i++)
+            {
+                Bullet bullet = Instantiate(bulletPrefab, gunPoint.position, gunPoint.GetChild(i).rotation) as Bullet;
+                bullet.m_Shooter = shooter;
+                bullet.rigidbody.velocity += velocity;
+            }
+            lastShotTime = Time.time;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
